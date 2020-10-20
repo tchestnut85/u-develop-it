c9065b0a35440ddce1f3274ea04f63f9f9ca8c22
Tom Chestnut <tchestnut85@gmail.com>
2020-10-20 15:10:13 -0400

    created routes to get all candidates and filter candidates by ID, and to delete a candidate by ID

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +8,86 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Connect to the SQL database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the election database.');
+});
+
+// GET all candidates
+// THIS METHOD IS THE KEY COMPONENT TO ALLOW SQL COMMANDS IN NODE.JS
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT *FROM candidateS
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.send(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// DELETE a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.send(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            data: this.changes
+        });
+    });
+});
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// //ES5 function (not an arrow function) to use the this keyword
+// db.run(sql, params, function (err, result) {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result, this.lastID);
+// });
+
 // Default response for any other request(Not Found) catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// Listener for PORT
-app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}!`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on Port ${PORT}!`);
+    });
 });
