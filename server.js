e96526a383e6cb853317a3471f88703eaa466d0a
Tom Chestnut <tchestnut85@gmail.com>
2020-10-20 15:58:26 -0400

    created a route to add a candidate to the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -41,7 +42,7 @@ app.get('/api/candidate/:id', (req, res) => {
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
-            res.send(400).json({ error: err.message });
+            res.status(400).json({ error: err.message });
             return;
         }
 
@@ -69,16 +70,29 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// //ES5 function (not an arrow function) to use the this keyword
-// db.run(sql, params, function (err, result) {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log(result, this.lastID);
-// });
+app.post('/api/candidate/', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 // Default response for any other request(Not Found) catch all
 app.use((req, res) => {
